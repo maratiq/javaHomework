7709a0953a0e8a62aaf5742afc33c9a28d369366
maratiq <zaripov2010m@gmail.com>
2020-03-05 22:45:38 +0400

    finished first task

---
+++ src/com/company/homeWork1/Main.java
@@ -0,0 +1,40 @@
+package com.company.homeWork1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Object obj = null;
+        int[] array;
+        int number = 3;
+        int value = 2;
+
+        /**
+         * @exception NullPointerException
+         * @author Zaripov
+         */
+
+//        if(obj.equals("NullPointerException")) {
+//            System.out.println("Hello world");
+//        }
+
+
+        /**
+         * @exception ArrayIndexOutOfBoundsException
+         * @author Zaripov
+         */
+
+//        array = new int[2];
+//        System.out.println("Hello " + array[2] + "World");
+
+        /**
+         * @exception IndexOutOfBoundsException
+         * @author Zaripov
+         */
+
+//        if (number > value) {
+//            throw new IndexOutOfBoundsException();
+//        }
+
+    }
+}
